224ac21d8f6dfe669f36b7af98dc1fbda4aa4bf1
Bruno Vaz <2010b9@gmail.com>
2024-09-10 21:57:08 +0100

    [WIP] Create blog page

---
+++ scripts/blog.js
@@ -0,0 +1,10 @@
+import {
+  initializeTypewriter,
+  initializeGranim,
+} from '../components/common.js';
+
+initializeTypewriter(
+  'app',
+  'I am a Lifelong Learner, Problem Solver, and Tech Enthusiast.'
+);
+initializeGranim('#canvas-image-blending', '../assets/walpaper.png');
